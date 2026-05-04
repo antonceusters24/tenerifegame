d1bb1dfbeb1eb1defff3b47b6c21a84e99bb3ae4
Anton Ceusters <anton.ceusters@allphi.eu>
2026-05-04 22:25:57 +0200

    chore: remove debug-env endpoint before prod merge

--- src/app/api/debug-env/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    NEXT_PUBLIC_USE_TEST_TABLES: process.env.NEXT_PUBLIC_USE_TEST_TABLES ?? "NOT SET",
-    isTrue: process.env.NEXT_PUBLIC_USE_TEST_TABLES === "true",
-  });
-}
